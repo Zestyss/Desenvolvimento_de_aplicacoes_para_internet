faa35ca04b7a1fdcc90031cb3d99e5eec65cf3f8
Anthony Gabriel <anthonygosdev@gmail.com>
2023-10-22 19:29:57 -0300

    Add files via upload

---
+++ aula_01_javascript.js
@@ -0,0 +1,28 @@
+// Demonstração JS
+var chapter = 'SCOPE in JavaScript';
+function Pessoal(){
+
+var nome = 'Exemplo in function';
+// ESCREVA UMA FUNÇÃO PARA O SEU PROJETO
+}
+function novoCadastro() {
+    console.log(chapter);
+    console.log(nome);
+    console.log(email);
+}
+novoCadastro();
+
+function endereço(){
+    console.log(cidade);
+    console.log(bairro);
+    console.log(rua);
+    console.log(numero);
+}
+endereço();
+
+function informaçõesEmpresa(){
+    console.log(cnpj);
+    console.log(telefone);
+    console.log(emaiEmpresarial)
+}
+informaçõesEmpresa();
